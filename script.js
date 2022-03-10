5979729ac2625cb0638d65d2cd11341b31f52203
Miti <101258918+MitiBsq@users.noreply.github.com>
2022-03-10 14:34:56 +0200

    random button

--- script.js
+++ script.js
@@ -9,16 +9,23 @@ let butoane=document.getElementById('butoane');
 let gameOver=document.getElementById('gameOver');
 let altecran = document.getElementById('altecran');
 let resetButton;
+let redcount;
+let greencount;
+let blackcount;
 
+
+
+// Buton Start Game
+userSubmitButton.addEventListener('click', startJoc);
 
 function startJoc() {
-        //dupa apasarea butoanelor scot "meniul"
+        //dupa apasarea butonului scot "meniul"
         ecran.remove();
         document.getElementById('startGame').remove();
 
 
-        // Creez cele 3 butoane
         ecranNou.textContent='Alege o culoare!'
+        // Creez cele 3 butoane
         let redButton= document.createElement('button');
         let greenButton= document.createElement('button');
         let blackButton= document.createElement('button');
@@ -33,58 +40,72 @@ function startJoc() {
         butoane.appendChild(blackButton);
 
         // Le adaug event
-        redButton.addEventListener('click', redButtonFunction);
-        greenButton.addEventListener('click', greenButtonFunction);
-        blackButton.addEventListener('click', blackButtonFunction);
-
-        // Creez functiile 
-        function redButtonFunction() {
-            altecran.textContent="Gresit "
-            
-            butoane.style.visibility='hidden'
-            joc.style.backgroundColor="red"
-            
-            gameOverMenu();
-        }
-        function greenButtonFunction() {
-            altecran.textContent="Felicitare ati castigat!"
-            butoane.style.visibility='hidden'
-            joc.style.backgroundColor="green"
-            
-            
-            gameOverMenu();
-        }
-        function blackButtonFunction() {
-            altecran.textContent="Mai incearca!"
-            butoane.style.visibility='hidden'
-            joc.style.backgroundColor="red"
-            
-            gameOverMenu();
-        }
-    
+        redButton.addEventListener('click', (redButtonFunction) =>{redcount=1; phase1()});
+        greenButton.addEventListener('click',(greenButtonFunction) =>{greencount=1; phase1()}); 
+        blackButton.addEventListener('click',(blackButtonFunction) =>{blackcount=1; phase1()});
 }
-// Buton Start Game
-userSubmitButton.addEventListener('click', startJoc);
+
+     function phase1(){
+             let randomButtons = generateRandomMove();
+             calculateWinner(randomButtons)
+            }
+
+     function generateRandomMove() {
+             let availableButtons = ['red','green','black'];
+             let randomNumber = Math.floor(Math.random() * availableButtons.length);
+             let whatbutton = availableButtons[randomNumber];
+            return whatbutton;}
+
+     function calculateWinner(randomButton) {
+            switch(randomButton){
+                case 'red':
+                    if(redcount===1)
+                        {ecranNou.textContent="Ati Castigat!";
+                         butoane.style.visibility='hidden';
+                        joc.style.backgroundColor="green";
+                         gameOverMenu();} 
+                    else {ecranNou.textContent="Ati Pierdut!";
+                         butoane.style.visibility='hidden'
+                        joc.style.backgroundColor="red"
+                         gameOverMenu();}
+                case 'green':
+                    if(greencount===1)  
+                        {ecranNou.textContent="Ati Castigat!";
+                        butoane.style.visibility='hidden';
+                        joc.style.backgroundColor="green";
+                        gameOverMenu(); } 
+                        else {ecranNou.textContent="Ati Pierdut!";
+                        butoane.style.visibility='hidden'
+                        joc.style.backgroundColor="red"
+                        gameOverMenu(); }
+                case 'black':
+                     if(blackcount===1)  
+                        {ecranNou.textContent="Ati Castigat!";
+                        butoane.style.visibility='hidden';
+                        joc.style.backgroundColor="green";
+                        gameOverMenu(); } 
+                        else {ecranNou.textContent="Ati Pierdut!";
+                        butoane.style.visibility='hidden'
+                        joc.style.backgroundColor="red"
+                        gameOverMenu();}    
+                }
+            }
 
 
 function gameOverMenu(){
-    ecranNou.style.display='none'
-    
     // Creez buton restart joc
     resetButton=document.createElement('button');
     resetButton.textContent="Restart Joc";
     resetButton.className="btn btn-lg btn-primary";
     gameOver.appendChild(resetButton);
-       
-    // functie reset joc
- function resetJoc(){
-        ecranNou.style.display='initial'
-        joc.style.backgroundColor="white"
-        altecran.textContent=''
-        butoane.style.visibility='visible'
-        resetButton.remove();
-        }
-
-        // Buton reset joc
-     resetButton.addEventListener('click', resetJoc)
+    // Buton reset joc
+    resetButton.addEventListener('click', resetJoc)
 }
+function resetJoc(){
+    ecranNou.textContent="Alege o culoare!"
+    joc.style.backgroundColor="white"
+    butoane.style.visibility='visible'
+    gameOver.visibility='hidden'
+    resetButton.remove();
+    }
+  
